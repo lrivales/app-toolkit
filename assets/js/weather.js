0c9b40d9720890057481597c1e0b1e5ea4dd3e20
Lawrence Rivales <lrivales@gmail.com>
2021-09-24 15:30:36 -0400

    added weather info functionality

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -3,21 +3,26 @@
 function getWeather() {
     // build queryUrl
     queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=Tokyo&APPID=7dddd78df5cbb905a33ba3be92421fbf&units=imperial"
-    
+
     fetch(queryUrl)
         .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            var city = (data.name);
-            console.log(city);
+        .then(currentWeather => {
+            // get city name
+            if (currentWeather.name) {
+                console.log(currentWeather)
+                // get city coordinates
+                var lat = (currentWeather.coord.lat);
+                var lon = (currentWeather.coord.lon);
+                $("#weatherModalTitle").text(currentWeather.name);
+                $("#weatherModalImg").attr("src", "https://openweathermap.org/img/wn/"+currentWeather.weather[0].icon+"@2x.png");
+                $("weatherModalImg").attr("alt", currentWeather.weather[0].description);
+                $("#weatherModalTemp").text("Temp: "+currentWeather.main.temp+"F");
+                $("#weatherModalWind").text("Wind: "+currentWeather.wind.speed+" MPH");
+                $("#weatherModalHumidity").text("Humidity: "+currentWeather.main.humidity+"%");
+            } else {
+                console.log("Invalid City Name");
+            }
         });
-  
-    //     .then(response => {
-    //         //do stuff
-    //         response.json()
-    //     })
-    // var city = (data.name);
-    // console.log(city)
 };
 
 $("#weatherBtn").click(function() {
@@ -25,11 +30,11 @@ $("#weatherBtn").click(function() {
     $("#weatherModal").addClass("active");
 
     // update modal for weather form
-
+    $("#weatherModalTitle").text("What city are you in?");
     // get city name
 
     // exec getWeather
-    getWeather();
+    // getWeather();
 
     // update modal with weather info
 });
